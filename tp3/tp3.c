f521a1b00289f5de82346df0495af6c4ba21918a
Hugo Resende <115218749+hugoffr@users.noreply.github.com>
2023-04-27 19:50:14 +0100

    Added tp3 (#9)
    * fixed overflow bug and small code smells
    
    * added tp3
    
    * Added sequential algorithm

--- tp3/tp3.c
+++ tp3/tp3.c
@@ -1,5 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void lu_factorization(double** A, int n) {
+    int i, j, k;
+    for (k = 0; k < n-1; k++) {
+        for (i = k+1; i < n; i++) {
+            double factor = A[i][k] / A[k][k];
+            for (j = k+1; j < n; j++) {
+                A[i][j] -= factor * A[k][j];
+            }
+            A[i][k] = factor;
+        }
+    }
+}
+
+void solve_lu(double** A, double* b, double* x, int n) {
+    int i, j;
+    double sum;
+    double* y = (double*) malloc(n * sizeof(double));
+    
+    // Forward substitution
+    y[0] = b[0];
+    for (i = 1; i < n; i++) {
+        sum = 0.0;
+        for (j = 0; j < i; j++) {
+            sum += A[i][j] * y[j];
+        }
+        y[i] = b[i] - sum;
+    }
+    
+    // Backward substitution
+    x[n-1] = y[n-1] / A[n-1][n-1];
+    for (i = n-2; i >= 0; i--) {
+        sum = 0.0;
+        for (j = i+1; j < n; j++) {
+            sum += A[i][j] * x[j];
+        }
+        x[i] = (y[i] - sum) / A[i][i];
+    }
+    
+    free(y);
+}
+
+void init_matrix_and_vector(double** A, double* b, int n) {
+    int i, j;
+    srand(time(NULL));
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            A[i][j] = (double) rand() / RAND_MAX;
+        }
+        b[i] = (double) rand() / RAND_MAX;
+    }
+}
+
+// This function should only be used to test the validity of the code
+void print_results(double** A, double* b, double* x, int n) {
+
+    int i, j;
+
+    // Print results
+    printf("Matrix A:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%f ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Vector b:\n");
+    for (i = 0; i < n; i++) {
+        printf("%f ", b[i]);
+    }
+
+    printf("\nSolution x:\n");
+    for (i = 0; i < n; i++) {
+        printf("%f ", x[i]);
+    }
+    printf("\n");
+}
+
+void sequential_solution(int n) {
+
+    clock_t tic, toc;
+
+    double** A = (double**) malloc(n * sizeof(double*));
+    double* b = (double*) malloc(n * sizeof(double));
+    double* x = (double*) malloc(n * sizeof(double));
+
+    for (int i = 0; i < n; i++) {
+        A[i] = (double*) malloc(n * sizeof(double));
+    }
+
+    init_matrix_and_vector(A, b, n);
+
+    tic = clock();
+    
+    // Perform LU factorization
+    lu_factorization(A, n);
+    
+    // Solve for x
+    solve_lu(A, b, x, n);
+
+    toc = clock();
+
+    printf("Sequential time for %d: %3.6f\n", n,(double)(toc - tic) / CLOCKS_PER_SEC);
+}
+
+int main() {
+
+    for (int n = 1024; n <= 8192; n = n + 1024) {
+        sequential_solution(n);
+    }
+}
+=======
 
 void LU_factorization(double** A, double** L, double** U, int n) {
     int i, j, k;
