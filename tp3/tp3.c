442cf765929d8e79c0befedbabe9fd029f34e72d
Joao Costa <joaoassuncaoecosta@gmail.com>
2023-05-17 22:29:49 +0100

    got block algorithm working

--- tp3/tp3.c
+++ tp3/tp3.c
@@ -3,22 +3,22 @@
 #include <time.h>
 
 
-
-void OnMultLine(double** A, double** B)
+/*
+void OnMultLine(double** A, double** B, int n)
 {
 	char st[100];
 	double temp;
 	int i, j, k;
-	for(i=0; i<m_ar; i++)
-	{	for( j=0; j<m_br; j++)
+	for(i=0; i<n; i++)
+	{	for( j=0; j<n; j++)
 		{
-			for( k=0; k<m_ar; k++)
+			for( k=0; k<n; k++)
 			{	
 				phc[i*m_ar+k] += pha[i*m_ar+k] * phb[j*m_br+k];
 			}
 		}
 	}
-}
+}*/
 
 
 void lu_factorization(double** A, int n) {
@@ -34,82 +34,126 @@ void lu_factorization(double** A, int n) {
     }
 }
 
-void block_lu_factorization(double** A, int n, int block_size){
 
-    //TODO Check for n non multiple of block_size
+void printMatrix(double** m, int n){
+    printf("Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void block_lu_factorization(double** A, int n, int block_size){
 
     if (block_size > n){
         lu_factorization(A, n);
         return;
     }
-    for (int pivot_block = 0; pivot_block < n/block_size+1; pivot_block++){
+    for (int pivot_block = 0; pivot_block < n/block_size; pivot_block++){
+        //printf("Starting pivot block %d\n",pivot_block);
+        //printMatrix(A,n);
         //We have to do this for each pivot block
         for (int pivot_i = 0; pivot_i < block_size; pivot_i++){
             double pivot = A[pivot_block*block_size+pivot_i][pivot_block*block_size+pivot_i];
+            //printf("Working on pivot %d: %lf\n",pivot_i,pivot);
 
             //Solve pivot block
             //Divide left column by pivot
-            for (int left_column_i = pivot_block*block_size+pivot_i; left_column_i < block_size; left_column_i++){
-                A[pivot_block * block_size + pivot_i][left_column_i] /= pivot;
+            for (int left_column_i = pivot_block*block_size+pivot_i; left_column_i < (pivot_block+1)*block_size; left_column_i++){
+                A[left_column_i][pivot_block * block_size + pivot_i] /= pivot;
+                //printf("Dividing element at (%d,%d) by pivot %f\n",left_column_i,pivot_block*block_size+pivot_i,pivot);
             }
+            //printMatrix(A,n);
+            //printf("Divided left line by pivot\n");
             //Update all remainig values
             for (int inner_mat_x = pivot_block*block_size+pivot_i+1; inner_mat_x < (pivot_block+1) * block_size; inner_mat_x++){
-                for (int inner_mat_y = pivot_block*block_size+pivot_i+1; inner_mat_y < (v_block_i+1) * block_size; inner_mat_y++){
+                for (int inner_mat_y = pivot_block*block_size+pivot_i+1; inner_mat_y < (pivot_block+1) * block_size; inner_mat_y++){
                     A[inner_mat_x][inner_mat_y] -= A[inner_mat_x][pivot_block*block_size+pivot_i] * A[pivot_block*block_size+pivot_i][inner_mat_y];
                 }
             }
+            //printMatrix(A,n);
+            //printf("Updated pivot block inner matrix\n");
 
 
             //Check if it is the last block
 
             //For each vertical block under the current pivot block
-            for(int v_block_i = pivot_block + 1 ; v_block_i < n/block_size+1;v_block_i ++){
+            //printf("Updating vertical blocks\n");
+            for(int v_block_i = pivot_block + 1 ; v_block_i < n/block_size;v_block_i ++){
+                //printf("Solving vertical block %d\n",v_block_i);
                 //Divide left column by pivot
                 for (int left_column_i = 0; left_column_i < block_size; left_column_i++){
-                    A[pivot_block*block_size+pivot_i][pivot_block*block_size+left_column_i] /= pivot;
+                    A[v_block_i*block_size+left_column_i][pivot_block*block_size+pivot_i] /= pivot;
+                    //printf("Divinding element at %d,%d (%f) by %f:  pivot_block=%d  v_block_i=%d  left_column_i=%d\n",pivot_block*block_size+pivot_i,v_block_i*block_size+left_column_i,A[pivot_block*block_size+pivot_i][pivot_block*block_size+left_column_i],pivot,pivot_block,v_block_i,left_column_i);
                 }
                 //Update all remainig values
                 for (int inner_mat_x = pivot_block*block_size+pivot_i+1; inner_mat_x < (pivot_block+1) * block_size; inner_mat_x++){
                     for (int inner_mat_y = v_block_i*block_size; inner_mat_y < (v_block_i+1) * block_size; inner_mat_y++){
-                        A[inner_mat_x][inner_mat_y] -= A[inner_mat_x][pivot_block*block_size+pivot_i] * A[pivot_block*block_size+pivot_i][inner_mat_y];
+                        //printf("Solving element at %d,%d\n",inner_mat_x,inner_mat_y);
+                        A[inner_mat_y][inner_mat_x] -= A[inner_mat_y][pivot_block*block_size+pivot_i] * A[pivot_block*block_size+pivot_i][inner_mat_x];
                     }
                 }
+                //printMatrix(A,n);
             }
 
             //For each horizontal block to the right of the pivot block
 
-            for(int h_block_i = pivot_block + 1 ; h_block_i < n/block_size+1;h_block_i ++){
+            //printf("Updating horizontal blocks\n");
+            for(int h_block_i = pivot_block + 1 ; h_block_i < n/block_size;h_block_i ++){
+                //printf("Solving horizontal block %d\n",h_block_i);
                 //Update all remainig values
-                for (int inner_mat_x = h_block_i*block_size; inner_mat_x < (h_block_i+1) * block_size; inner_mat_x++){
+                for (int inner_mat_x = (h_block_i+pivot_block)*block_size; inner_mat_x < (pivot_block + h_block_i+1) * block_size; inner_mat_x++){
                     for (int inner_mat_y = pivot_block*block_size+pivot_i+1; inner_mat_y < (pivot_block+1) * block_size; inner_mat_y++){
-                        A[inner_mat_x][inner_mat_y] -= A[inner_mat_x][pivot_block*block_size+pivot_i] * A[pivot_block*block_size+pivot_i][inner_mat_y];
+                        //printf("Analysing %d,%d\n",inner_mat_x,inner_mat_y);
+                        A[inner_mat_y][inner_mat_x] -= A[inner_mat_y][pivot_block*block_size+pivot_i] * A[pivot_block*block_size+pivot_i][inner_mat_x];
                     }
                 }
+                //printMatrix(A,n);
             }
 
+            //printf("Updating the rest of the matrix\n");
+            //printMatrix(A,n);
             //Set each inner block as the multiplication of its upper most and left most blocks
-            for(int h_block_i = pivot_block+1;h_block_i < n/block_size+1;h_block_i ++){
-                for(int v_block_i = pivot_block+1;v_block_i < n/block_size+1;v_block_i ++){
+            for(int h_block_i = pivot_block+1;h_block_i < n/block_size;h_block_i ++){
+                for(int v_block_i = pivot_block+1;v_block_i < n/block_size;v_block_i ++){
+                    //printf("Solving block %d,%d\n",h_block_i,v_block_i);
+                    for (int inner_mat_x = (h_block_i)*block_size; inner_mat_x < (h_block_i+1) * block_size; inner_mat_x++){
+                        for (int inner_mat_y = v_block_i*block_size; inner_mat_y < (v_block_i+1) * block_size; inner_mat_y++){
+                            A[inner_mat_y][inner_mat_x] -= A[inner_mat_y][pivot_block*block_size+pivot_i] * A[pivot_block*block_size+pivot_i][inner_mat_x];
+                        }
+                    }
 
+                    /*
                     //Start by zeroing the inner block
                     for(int block_x = h_block_i * block_size; block_x < (h_block_i+1)*block_size; block_x++){
                         for(int block_y = h_block_i * block_size; block_y < (h_block_i+1)*block_size; block_y++){
-                            A[block_x][block_y] = 0;
+                            A[block_y][block_x] = 0;
                         }
                     }
 
                     //Multiply the matrices by line
-
                     for(int line_i = 0; line_i < block_size; line_i++){
                         for (int left_mat_i = 0; left_mat_i < block_size; left_mat_i++){
                             for (int top_mat_i = 0; top_mat_i < block_size; top_mat_i++){
-                                A[pivot_block*block_size+left_mat_i][pivot_block*block_size+line_i + left_mat_i] += A[pivot_block*block_size+left_mat_i][block_y+line_i] * A[block_x+top_mat_i][pivot_block*block_size+line_i+left_mat_i];
+                                A[pivot_block*block_size+line_i + left_mat_i][pivot_block*block_size+left_mat_i] += A[pivot_block*block_size+left_mat_i][v_block_i*block_size+line_i] * A[h_block_i*block_size+top_mat_i][pivot_block*block_size+line_i+left_mat_i];
                             }
                         }
                     }
+
+                    for(int left_matrix_line = 0; left_matrix_line < block_size; left_matrix_line++){
+                        for(int top_matrix_line = 0; top_matrix_line < block_size; top_matrix_line++){
+                            for(int column_i = 0; column_i < block_size; column_i++){
+                                A[v_block_i*block_size + left_matrix_line][h_block_i*block_size + column_i] += A[v_block_i*block_size+left_matrix_line][pivot_block* block_size + top_matrix_line]
+                            }
+                        }
+                    }
+                    */
                 }
             }
-
+            //printf("Updated Matrix:\n");
+            //printMatrix(A,n);
         }
     }
 }
@@ -209,7 +253,21 @@ void sequential_solution(int n) {
 
 int main() {
 
-    for (int n = 1024; n <= 8192; n = n + 1024) {
+    /*for (int n = 1024; n <= 8192; n = n + 1024) {
         sequential_solution(n);
+    }*/
+
+    int n=9;
+    double** A = (double**) malloc(n * sizeof(double*));
+    for(int i = 0; i < n; i++){
+        A[i] = (double*)malloc(n*sizeof(double));
+        for(int j = 0; j < n; j++){
+            A[i][j] = (double)(rand()%100-50);
+            //A[i][j] = (double)(1+2*i+j);
+        }
     }
+
+    block_lu_factorization(A, n, 3);
+
+    printMatrix(A,n);
 }
